fab35dec3dcf064e6bd20cd878c5ccdd1c6e8095
RowDog <mrdiamonddirt@gmail.com>
2022-11-22 12:50:09 +0000

    crud functions

---
+++ src/user/userControllers.js
@@ -0,0 +1,113 @@
+const { response } = require("express");
+const User = require("../models/userModel");
+
+// create new user
+exports.createUsers = async (req, res) => {
+    console.log(req.body);
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({
+            status: "success",
+            data: {
+                user,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
+    }
+};
+
+// return all users
+exports.getAllUsers = async (req, res) => {
+    console.log(req.body);
+    try {
+        const users = await User.findAll();
+        res.status(200).json({
+            status: "success",
+            results: users.length,
+            data: {
+                users,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err,
+        });
+    }
+}
+
+// get one user
+exports.getUser = async (req, res) => {
+    console.log(req.params);
+    try {
+        const user = await User.findByPk(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: {
+                user,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err,
+        });
+    }
+}
+
+// delete user
+exports.deleteUser = async (req, res) => {
+    console.log(req.params);
+    try {
+        await User.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(204).json({
+            status: "success",
+            data: null,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err,
+        });
+    }
+}
+
+// login user
+exports.loginUser = async (req, res) => {
+    console.log(req.body);
+    try {
+        if (req.body.email && req.body.password) {
+            const user = await User.findOne({
+                where: {
+                    email: req.body.email,
+                    password: req.body.password,
+                },
+            });
+            response.status(200).json({
+                status: "success",
+                data: {
+                    user,
+                },
+            });
+        } else {
+            res.status(400).json({
+                status: "fail",
+                message: "Please enter email and password",
+            });
+        }
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err,
+        });
+        
+    }
+}
---
+++ src/user/userModel.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/connection");
+
+// sequalize auto to auto generate the table
+
+const User = sequelize.define("User", {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    // role: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    // },
+    // createdAt: {
+    //     type: DataTypes.DATE,
+    //     allowNull: false,
+    // },
+    // updatedAt: {
+    //     type: DataTypes.DATE,
+    //     allowNull: false,
+    // },
+});
+
+module.exports = User;
