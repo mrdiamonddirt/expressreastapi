bb52ae57e7a1e7766584e16d55f20d5010c700ea
RowDog <mrdiamonddirt@gmail.com>
2022-11-22 12:52:14 +0000

    routes

---
+++ src/user/userRouter.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const {createUsers, getAllUsers, getUser} = require('../controllers/userControllers');
+const userRouter = Router();
+
+
+userRouter.post('/adduser', createUsers);
+userRouter.get('/getusers', getAllUsers);
+userRouter.get('/getuser/:id', getUser);
+userRouter.get('/getuser', getUser);
+userRouter.delete('/deleteuser/:id', getUser);
+
+module.exports = userRouter;
