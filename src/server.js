232e028ef8de749f4449ee9282d49888836b873a
RowDog <mrdiamonddirt@gmail.com>
2022-12-13 12:41:03 +0000

    add socket io and listen for connect

--- src/server.js
+++ src/server.js
@@ -3,32 +3,42 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./user/userRouter");
 const { User } = require("./user/userModel");
+const app = express();
 // const favoriteRouter = require("./favorite/favoriteRouter");
 // const { Favorite } = require("./favorite/favoriteModel");
 // port connection
 const port = process.env.PORT || 5001;
+// socket io port
+const socketPort = 5005;
 
 // express app
-const app = express();
+
 app.use(cors());
+
+// socket io connection
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+server.listen(socketPort, () => {
+    console.log(`Socket io is running on port ${socketPort}`);
+});
 
 app.use(express.json());
 app.use(userRouter);
+// app.use(favoriteRouter);
 
-// async function relationships() {
-//     User.hasMany(Favorite, {
-//         foreignKey: "user_id",
-//         sourceKey: "id",
-//     });
-//     Favorite.belongsTo(User);
-
-//     sequelize.sync();
-// }
-// relationships();
-
-// get health from server
 app.get("/health", (req, res) => {
     res.status(200).send({ message: "API is working" });
+});
+
+// listen for socket io connection
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 
 // sequelize connection
